e7ce5e340cd8d70cb3c8baca24969f6562fe4094
Arthur <arthurerconggao@gmail.com>
2019-07-19 11:00:55 -0600

    Update share_bike.py

--- PY_Scripts/share_bike.py
+++ PY_Scripts/share_bike.py
@@ -333,14 +333,14 @@ def main():
     # print("avg_car: ",avg_car)
 
 
-def station_125(data_path, goPath,carbon_list,distance_list):
+def station_125(data_path, goPath,carbon_list,distance_list,k):
     #carbon_list = []
     #distance_list = []
     
     if True:
         stations = generate_data(data_path)
         # tsp = get_tsp(stations,Station(0,9999,depot_x,depot_y))
-        carbon, sum_distance = generate_carbon(stations,k)
+        carbon, sum_distance = generate_carbon(stations, k)
         carbon_list.append(carbon)
         distance_list.append(sum_distance)
         
@@ -392,22 +392,22 @@ def net_function():
         for i in range(0,nCsv[l.index(data_path)]):
 
             Data_path = data_path + str(i) +".csv"
-            carbon_list,distance_list = station_125(Data_path,data_path,carbon_list,distance_list)
+            carbon_list,distance_list = station_125(Data_path,data_path,carbon_list,distance_list, 0)
 
-        write_csv(0,carbon_list,distance_list)
+        write_csv(0,data_path,carbon_list,distance_list)
         carbon_list = []
         distance_list = []
         for i in range(0,nCsv[l.index(data_path)]):
             Fata_path = data_path + str(i) +".csv"
-            carbon_list,distance_list = station_125(Fata_path,data_path,carbon_list,distance_list)
+            carbon_list,distance_list = station_125(Fata_path,data_path,carbon_list,distance_list, 5)
 
-        write_csv(5,carbon_list,distance_list)
+        write_csv(5,data_path,carbon_list,distance_list)
         carbon_list = []
         distance_list = []
         for i in range(0,nCsv[l.index(data_path)]):
             Kata_path = data_path + str(i) +".csv"
-            carbon_list,distance_list = station_125(Kata_path,data_path,carbon_list,distance_list)
-        write_csv(10,carbon_list,distance_list)
+            carbon_list,distance_list = station_125(Kata_path,data_path,carbon_list,distance_list, 10)
+        write_csv(10,data_path,carbon_list,distance_list)
             
 # main()
 
